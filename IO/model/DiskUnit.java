fce2fa1fa7a7757d4bd91fec4d8167a5f3304960
Efterklang <gaojiaxing2004@qq.com>
2025-03-16 19:31:55 +0800

    doc: diskunit添加docstring

--- IO/model/DiskUnit.java
+++ IO/model/DiskUnit.java
@@ -3,7 +3,10 @@ package IO.model;
 import java.util.List;
 
 /**
- * 写命令-位置描述
+ * DiskUnit用于描述对象的某一个副本写入的位置
+ * 
+ * @example <code>DiskUnit diskUnit = new DiskUnit(0, List.of(1, 2, 3));</code> 
+ * 表示副本写入到磁盘0的1, 2, 3单元
  */
 public class DiskUnit {
     public int diskId; // 磁盘ID
