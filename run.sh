52cebcd1387fbbb47e809ae9c14e8dc6b6b55ffb
nbb <nbbwork@foxmail.com>
2025-03-16 22:33:48 +0800

    feat: 更新脚本-big跑大数据集

--- run.sh
+++ run.sh
@@ -8,12 +8,21 @@ for arg in "$@"; do
         break
     fi
 done
+# 检查是否有-big参数
+INPUT_FILE="./test/sample.in"
+for arg in "$@"; do
+    if [ "$arg" = "-big" ]; then
+        INPUT_FILE="./test/sample_practice.in"
+        break
+    fi
+done
+
 
 # 检测操作系统类型
 if [ "$(uname -s)" = "Darwin" ]; then
     # Mac系统
-    python3 ./test/run.py ./test/interactor_mac ./test/sample.in "java -cp ./build Main" $DEBUG_OPTION
+    python3 ./test/run.py ./test/interactor_mac $INPUT_FILE "java -cp ./build Main" $DEBUG_OPTION
 else
     # 其他系统
-    python3 ./test/run.py ./test/interactor ./test/sample.in "java -cp ./build Main" $DEBUG_OPTION
+    python3 ./test/run.py ./test/interactor $INPUT_FILE "java -cp ./build Main" $DEBUG_OPTION
 fi
