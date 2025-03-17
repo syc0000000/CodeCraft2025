35132c9634acaf8b69a0c4d14fb8f22a964bcc12
HarderHeng <l15069465372@163.com>
2025-03-17 22:27:27 +0800

    feat: 封装reader中添加任务函数，限制工作范围函数，更正Readonly策略

--- Reader/ReadOnlyStrategy.java
+++ Reader/ReadOnlyStrategy.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Map;
 import IO.model.ReadCommandIn;
 import IO.model.ReadCommandOut;
@@ -21,21 +22,8 @@ public class ReadOnlyStrategy implements ReaderStrategy {
     @Override
     public ReadRetrun read(ArrayList<ReadCommandIn> readCommandIns) {
         ReadRetrun readRetrun = new ReadRetrun();
-        // 填入ReadTask
-        for (ReadCommandIn readCommandIn : readCommandIns) {
-            UserObject object = Info.objMap.get(readCommandIn.objId);
-            ReadTask readTask = new ReadTask(readCommandIn.commandId, readCommandIn.objId, object.objSize);
-            Info.readTaskTbl.put(readCommandIn.commandId, readTask);
-            readerLogger.debug("加入objTaskMap: " + readCommandIn.objId + " " +
-                    readCommandIn.commandId);
-            if (Info.objTaskMap.get(readCommandIn.objId) == null) {
-                HashSet<Integer> readTaskSet = new HashSet<>();
-                readTaskSet.add(readCommandIn.commandId);
-                Info.objTaskMap.put(readCommandIn.objId, readTaskSet);
-            } else {
-                Info.objTaskMap.get(readCommandIn.objId).add(readCommandIn.commandId);
-            }
-        }
+        //添加所有的任务
+        addReadTask(readCommandIns);
         // 每TickToken
         int tickToken = Info.tokenPerTick;
         // readerLogger.debug("tokenPerTick: " + tickToken);
@@ -69,15 +57,14 @@ public class ReadOnlyStrategy implements ReaderStrategy {
                         // readerLogger.debug("objId: " + objId);
                         // 遍历unit list 获取这一格是obj的第几个分片
                         int blockId = disk.unitData.get(disk.ptr).blockId;
-                        HashSet<Integer> taskSet = Info.objTaskMap.get(objId);
+                        LinkedList<ReadTask> taskSet = obj.readTasks;
                         if (taskSet != null) {
-                            Iterator<Integer> iterator = taskSet.iterator();
+                            Iterator<ReadTask> iterator = taskSet.iterator();
                             while (iterator.hasNext()) {
-                                Integer task = iterator.next();
-                                ReadTask readTask = Info.readTaskTbl.get(task);
+                                ReadTask readTask = iterator.next();
                                 boolean addSucc = readTask.blockFinished.add(blockId);
                                 boolean removeSucc = readTask.blockNotFinished.remove(blockId);
-                                readerLogger.debug("taskId: " + task + "完成块: " + blockId + " addSucc: "
+                                readerLogger.debug("taskId: " + readTask.taskId + "完成块: " + blockId + " addSucc: "
                                         + addSucc + ", removeSucc: " + removeSucc);
                                 if (readTask.blockNotFinished.isEmpty()) {
                                     // 任务完成
