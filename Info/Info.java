2f49781a02f95dfa8ffd266bbc35a9912e85843d
Efterklang <gaojiaxing2004@qq.com>
2025-03-18 23:18:41 +0800

    fix: 遍历space而非unit

--- Info/Info.java
+++ Info/Info.java
@@ -415,14 +415,17 @@ public class Info {
         }
         
         /**
-         * release的space恰好是RWSpace的最后一个空间，此时需要更新RWEnd
+         * 作为releaseSpace的辅助方法
+         * 当release的space恰好是RWSpace的最后一个空间，调用该方法更新RWEnd；
          * 
          * @param space 释放的space，它恰好是RWSpace的最后一个空间
          */
         private void updateRWEndAfterRelease(DiskSpace space) {
-            int prevUnitIndex = space.start - 1; // 
-            while (unitData.get(prevUnitIndex).objId < 0) {
-                prevUnitIndex--;
+            int prevUnitIndex = space.start - 1;
+            DiskSpace prevDiskSpace = space.start > 0 ? unitData.get(prevUnitIndex).space : null;
+            while (prevDiskSpace != null && prevDiskSpace.isFree) {
+                prevUnitIndex = prevDiskSpace.start - 1;
+                prevDiskSpace = prevUnitIndex >= 0 ? unitData.get(prevUnitIndex).space : null;
             }
             log.debug("释放space后更新RWEnd: " + RWEnd + " -> " + prevUnitIndex);
             RWEnd = prevUnitIndex;
