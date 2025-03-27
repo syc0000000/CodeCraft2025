05abba6697c165a5e2386254fe553d8fabf73292
suyoucha <suyoucha@minimaxi.com>
2025-03-27 13:47:05 +0800

    chore: 格式化

--- Reader/NewReaderStratrgy.java
+++ Reader/NewReaderStratrgy.java
@@ -22,6 +22,7 @@ import Info.Info.UserObject;
 
 public class NewReaderStratrgy implements ReaderStrategy {
     private static final int[] R_COSTS = { 64, 52, 42, 34, 28, 23, 19, 16 };
+
     @Override
     public ReadRetrun read(ArrayList<ReadCommandIn> readCommandIns) {
         // TODO: 实现默认的读取策略
@@ -31,7 +32,7 @@ public class NewReaderStratrgy implements ReaderStrategy {
         int tickToken = Info.tokenPerTick;
         Map<Integer, ReadCommandOut> readCommandOuts = new HashMap<>();
         HashSet<CompleteCommandOut> completeCommandOuts = new HashSet<>();
-        //readerLogger.debug("进入读取模块");
+        // readerLogger.debug("进入读取模块");
         // 遍历磁盘
         for (int i = 0; i < Info.diskNum; i++) {
             // 基础准备
@@ -44,16 +45,16 @@ public class NewReaderStratrgy implements ReaderStrategy {
                 readCommandOut.jumpTarget = 0;
                 disk.ptr = 0;
                 readCommandOuts.put(i, readCommandOut);
-                //readerLogger.debug("磁盘编号" + i + "已经跳转");
+                // readerLogger.debug("磁盘编号" + i + "已经跳转");
                 continue;
             }
-            //准备优化的读取序列
+            // 准备优化的读取序列
             ArrayList<Action> sequence = new ArrayList<>();
-            int k = 0; //向前加入READ序列
-            int tokencpy = tokenNow; //token的拷贝
-            int tokenRead = 0; //在这个序列中已经使用过的token数量
-            int newtoken = 0; //新的token
-            int pasttoken = 1;  //上一次的token
+            int k = 0; // 向前加入READ序列
+            int tokencpy = tokenNow; // token的拷贝
+            int tokenRead = 0; // 在这个序列中已经使用过的token数量
+            int newtoken = 0; // 新的token
+            int pasttoken = 1; // 上一次的token
             switch (disk.pretoken) {
                 case 64:
                     k = 1;
@@ -104,31 +105,30 @@ public class NewReaderStratrgy implements ReaderStrategy {
                 default:
                     break;
             }
-            //将额外操作的token加入
-            for(int temp = 0; temp < k; temp++){
+            // 将额外操作的token加入
+            for (int temp = 0; temp < k; temp++) {
                 tokenRead += R_COSTS[temp];
             }
-            //添加从目前位置向后的序列
-            int pretoken = disk.pretoken; //手动添加序列操作中，上一次的token
-            Action preoper = disk.preoper; //手动添加序列操作中，上一次的操作
-            int sequenceptr = 0; //手动添加序列操作中，用到的指针
+            // 添加从目前位置向后的序列
+            int pretoken = disk.pretoken; // 手动添加序列操作中，上一次的token
+            Action preoper = disk.preoper; // 手动添加序列操作中，上一次的操作
+            int sequenceptr = 0; // 手动添加序列操作中，用到的指针
             Result result = new Result();
-            while(newtoken != pasttoken){
-                //添加未优化路径
-                while(tokencpy > 0){
-                    if(disk.unitData.get(disk.ptr + sequenceptr).isInTask){
+            while (newtoken != pasttoken) {
+                // 添加未优化路径
+                while (tokencpy > 0) {
+                    if (disk.unitData.get(disk.ptr + sequenceptr).isInTask) {
                         int tokenIsToUse = calculateToken(Info.Action.READ, preoper, pretoken);
-                        if(tokencpy - tokenIsToUse < 0){
+                        if (tokencpy - tokenIsToUse < 0) {
                             break;
                         }
                         pretoken = tokenIsToUse;
                         sequence.add(Info.Action.READ);
                         tokencpy -= pretoken;
                         preoper = Info.Action.READ;
-                    }
-                    else{
+                    } else {
                         int tokenIsToUse = calculateToken(Info.Action.PASS, preoper, pretoken);
-                        if(tokencpy - tokenIsToUse < 0){
+                        if (tokencpy - tokenIsToUse < 0) {
                             break;
                         }
                         pretoken = tokenIsToUse;
@@ -136,17 +136,17 @@ public class NewReaderStratrgy implements ReaderStrategy {
                         tokencpy -= pretoken;
                         preoper = Info.Action.PASS;
                     }
-                    
+
                     sequenceptr++;
                 }
 
                 pasttoken = tokenRead + tokenNow - tokencpy;
                 result = SequenceOptimizer.optimizeSequence(sequence);
                 newtoken = result.cost;
-                tokencpy += pasttoken - newtoken;            
+                tokencpy += pasttoken - newtoken;
             }
-            //将最优序列添加到输出中
-            for(int temp = k; temp < result.sequence.size(); temp++){
+            // 将最优序列添加到输出中
+            for (int temp = k; temp < result.sequence.size(); temp++) {
                 readCommandOut.actions.add(result.sequence.get(temp));
             }
             for (int j = 0; j < readCommandOut.actions.size(); j++) {
@@ -155,17 +155,19 @@ public class NewReaderStratrgy implements ReaderStrategy {
                     int blockId = disk.unitData.get(disk.ptr + j).blockId;
                     int objId = disk.unitData.get(disk.ptr + j).objId;
                     UserObject obj = Info.objMap.get(objId);
-                    //readerLogger.debug("ptr为"+(disk.ptr+j)+"已读到objid为" + objId + " obj为" + obj + " blockid为" + blockId);
+                    // readerLogger.debug("ptr为"+(disk.ptr+j)+"已读到objid为" + objId + " obj为" + obj +
+                    // " blockid为" + blockId);
                     Iterator<ReadTask> iterator = obj.readTasks.iterator();
                     while (iterator.hasNext()) {
                         ReadTask readTask = iterator.next();
                         if (readTask.blockNotFinished.contains(blockId)) {
                             // 检测任务的完成
                             // 检测过期
-                            //readerLogger.debug("任务ID，objid，blockid为" + readTask.taskId + "," + objId + "," + blockId);
+                            // readerLogger.debug("任务ID，objid，blockid为" + readTask.taskId + "," + objId +
+                            // "," + blockId);
                             // readerLogger.debug("任务是否完成"+readTask.blockNotFinished.isEmpty());
                             if (readTask.isTimeout()) {
-                                //readerLogger.debug("任务过期: " + readTask.taskId);
+                                // readerLogger.debug("任务过期: " + readTask.taskId);
                                 obj.timeoutTasks.add(readTask.taskId);
                                 iterator.remove();
                                 continue;
@@ -176,7 +178,7 @@ public class NewReaderStratrgy implements ReaderStrategy {
                             readTask.blockFinished.add(blockId);
                             // readerLogger.debug("任务是否完成"+readTask.blockNotFinished.isEmpty());
                             if (readTask.blockNotFinished.isEmpty()) {
-                                //readerLogger.debug("上报任务id " + readTask.taskId + " objID" + objId);
+                                // readerLogger.debug("上报任务id " + readTask.taskId + " objID" + objId);
                                 completeCommandOuts.add(new CompleteCommandOut(readTask.taskId));
                                 iterator.remove();
                             }
@@ -184,19 +186,21 @@ public class NewReaderStratrgy implements ReaderStrategy {
                     }
                 }
             }
-            //更新硬盘信息
+            // 更新硬盘信息
             disk.preoper = preoper;
             disk.pretoken = pretoken;
             disk.ptr += sequenceptr;
 
             // 如果检测到需要跳转，则直接跳转
-            //readerLogger.debug("磁盘编号" + i + "目前ptr位置为" + disk.ptr + "RWEnd位置为" + disk.RWEnd);
+            // readerLogger.debug("磁盘编号" + i + "目前ptr位置为" + disk.ptr + "RWEnd位置为" +
+            // disk.RWEnd);
             readCommandOuts.put(i, readCommandOut);
         }
         readRetrun.readCommandOuts = readCommandOuts;
         readRetrun.completeCommandOuts = completeCommandOuts;
         return readRetrun;
     }
+
     public int calculateToken(Info.Action action, Action preoper, int pretoken) {
         switch (action) {
             case READ:
@@ -217,5 +221,5 @@ public class NewReaderStratrgy implements ReaderStrategy {
                 return -1;// 异常
         }
     }
-    
+
 }
